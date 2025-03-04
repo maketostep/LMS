58d473955cb2ceba31e21183bf2f71ff1e2709e9
Мартун Мкртчян <mkrtchyan@cpoz.local>
2025-03-04 11:49:53 +0300

    backup

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -23,34 +23,19 @@ const Home = () => {
             ОСУЩЕСТВЛЕНИЯ ЗАКУПОК"
           </Typography>
         </Box>
-        <Outlet />
-        <Grid2 container spacing={4}>
-          <Grid2 item xs={12} sm={6} md={4}>
-            <Paper elevation={3} sx={{ p: 2 }}>
-              <Typography variant="h5">
-                Учитесь в своем собственном темпе
-              </Typography>
-              <Typography variant="body1">
-                Вы можете получить доступ к нашим курсам в любое время и в любом
-                месте.
-              </Typography>
-            </Paper>
-          </Grid2>
-          <Grid2 item xs={12} sm={6} md={4}>
-            <Paper elevation={3} sx={{ p: 2 }}>
-              <Typography variant="h5">Опытные инструкторы</Typography>
-              <Typography variant="body1">
-                Учитесь у экспертов отрасли с реальным опытом.
-              </Typography>
-            </Paper>
-          </Grid2>
-          <Grid2 item xs={12} sm={6} md={4}>
-            <Paper elevation={3} sx={{ p: 2 }}>
-              <Typography variant="h5">Круглосуточный доступ</Typography>
-              <Typography variant="body1">Учитесь 24/7.</Typography>
-            </Paper>
-          </Grid2>
-        </Grid2>
+        <Box
+          component={Link}
+          to="/auth"
+          my={4}
+          sx={{
+            p: 3,
+            alignItems: "center",
+            alignContent: "center",
+            alignSelf: "center",
+          }}
+        >
+          <Button variant="outlined">Вход</Button>
+        </Box>
       </Container>
     </>
   );
--- vite-project/src/utils/App.jsx
+++ vite-project/src/utils/App.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router";
 const App = () => {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Outlet />
     </>
   );
